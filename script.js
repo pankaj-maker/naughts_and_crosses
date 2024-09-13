4fe85bb1bf81072f19d464b4a5e710a1711b9cbd
pankaj-maker <Pankaj14321@protonmail.com>
2024-09-13 10:54:45 +0000

    utility clasess

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
-const currentPlayerEl=document.querySelector(".current-player");
-const gameGrid=document.querySelectorAll(".box");
+const currentPlayerEl = document.querySelector(".current-player");
+const gameGrid = document.querySelectorAll(".box");
+const resetBtnEl = document.querySelector(".reset-btn");
+const gameGridd = document.querySelector(".game");
 
+resetBtnEl.classList.add("active");
+// resetBtnEl.addEventListener("click",()=>{
+//     resetBtnEl.classList.remove("active")
+// });
 
-function handleClick(el){
-    el.textContent=currentPlayer
-    swapPlayer();
-    
+const color = ["red", "green", "blue", "yellow", "black"];
+function handleClick() {
+//  const rn = Math.floor(Math.random() * color.length);
+  gameGridd.style.background = randomHex();
+}
+resetBtnEl.addEventListener("click", handleClick);
+
+// function handleClick(el) {
+//   el.textContent = currentPlayer;
+//   swapPlayer();
+// }
+
+function randomHex() {
+    const char="0123456789abcdef";
+    let hex="#"
+    for (let  i= 0; i < 6; i++) {
+    const randomchar=char[Math.floor(Math.random()*char.length)];
+    hex += randomchar
+    }
+    return hex
 }
 
-gameGrid.forEach((el)=>{
-el.addEventListener("click",()=>{
-    handleClick(el)
-})
-})
+gameGrid.forEach((el) => {
+  el.addEventListener("click", () => {
+    handleClick(el);
+  });
+});
 
-const player=["O","X"];
+const player = ["O", "X"];
 let currentPlayer;
 
-const randomPlayer=function(){
-    const randomNo=Math.floor(Math.random()*2)
-    currentPlayer=player[randomNo]
-}
+const randomPlayer = function () {
+  const randomNo = Math.floor(Math.random() * 2);
+  currentPlayer = player[randomNo];
+};
 randomPlayer();
-currentPlayerEl.textContent=currentPlayer;
+currentPlayerEl.textContent = currentPlayer;
 console.log(currentPlayer);
 
-
-
-
-
-const swapPlayer=()=>{
-    if(currentPlayer==="X"){
-        currentPlayer= "O"
-    }else{
-        currentPlayer= "X"
-    }
-}
-
-
+const swapPlayer = () => {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+};
